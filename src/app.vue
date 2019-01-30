ebf98f221cce3de760e0959a8dd55624cbfafd9e
YukiYonekura <yuki.yonekura@myu-on.co.jp>
2019-01-30 11:14:51 +0900

    Change to vue-cli3

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,26 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+#app
+  #nav
+    router-link(to='/') Home
+    |  |
+    router-link(to='/about') About
+  router-view
 </template>
 
-<script lang="ts" src="./app.ts">
-</script>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+#nav
+  padding 30px
+  a
+    font-weight bold
+    color #2c3e50
+    &.router-link-exact-active
+      color #42b983
 </style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.home
+  img(alt='Vue logo', src='../assets/logo.png')
+  hello-world(msg='Welcome to Your Vue.js + TypeScript App')
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+export default Vue.extend({
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
